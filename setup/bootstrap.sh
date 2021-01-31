7c4a1206d945537feb5e05ec81d790b2625b63b1
David Kolb <david.c.kolb@gmail.com>
2021-01-31 15:24:09 -0500

    Add deployments directory and initial flux2 objects.

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,42 +1,26 @@
 #!/bin/bash
 
-echo "Installing flux"
-
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-echo "Applying namespace yaml..."
+echo "Bootstrapping flux..."
 
-kubectl get namespace flux
-FLUX_NS_EXISTS=$?
+pushd $REPO_ROOT/deployments
+mkdir -p flux-system/
 
-if [ "$FLUX_NS_EXISTS" -ne 0 ]; then
-  kubectl create namespace flux
-fi
+flux install --version=latest \
+  --watch-all-namespaces \
+  --export > ./flux-system/gotk-components.yaml
 
-echo "Creating ssh key..."
+popd
 
-KEYFILE=$(tempfile)
-rm "$KEYFILE"
+kubectl apply -f "$REPO_ROOT"/deployments/flux-system/gotk-components.yaml
 
-kubectl -n flux get secret flux-ssh
-SSH_SECRET_EXISTS=$?
+flux create source git flux-system \
+  --url=ssh://git@github.com/dkolb/pi-k8s-cluster \
+  --branch=main
 
-if [ "$SSH_SECRET_EXISTS" -ne 0 ]; then
-  ssh-keygen -q -N "" -f "$KEYFILE"
-  kubectl -n flux delete secret generic flux-ssh --ignore-not-found
-  kubectl -n flux create secret generic flux-ssh --from-file="deploy-key=$KEYFILE" --from-file="pub-key=$KEYFILE.pub"
-  echo "Add the following to your repo as a deployment key with write access."
-  echo "==================BEGIN PUBLIC KEY=================="
-  cat "$KEYFILE.pub"
-  echo "================== END PUBLIC KEY =================="
-
-  read -p "Press enter to continue..."
-
-  rm "$KEYFILE"*
-fi
-
-echo "Installing flux helm chart..."
-
-helm repo add fluxcd https://charts.fluxcd.io
-helm repo update
-helm upgrade --install flux --values "${REPO_ROOT}/deployments/flux/flux/flux-values.yaml" --namespace flux fluxcd/flux
+flux create kustomization flux-system \
+  --source=flux-system \
+  --path="./deployments" \
+  --prune=true \
+  --interval=10m
